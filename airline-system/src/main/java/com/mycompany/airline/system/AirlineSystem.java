80a43ec670df52c261a8d1c2e9fae6402e6b7515
Alexander Esquivel Barrientos <esq.bar.alex@gmail.com>
2024-03-17 16:02:47 -0600

    project init

---
+++ airline-system/src/main/java/com/mycompany/airline/system/AirlineSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airline.system;
+
+/**
+ *
+ * @author esqba
+ */
+public class AirlineSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
